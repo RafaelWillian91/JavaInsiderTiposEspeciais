fb165ec4273ed9977b340f63759299f81179b7f0
Rafa <rafa.willian91@gmail.com>
2023-02-17 20:52:01 -0300

    Substituindo uma String por outra

---
+++ src/SubstituindoUmTextoDeUmaStringReplace/main.java
@@ -0,0 +1,16 @@
+package SubstituindoUmTextoDeUmaStringReplace;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        String str = "Java is the greatest language!";
+
+        String st1 = str.replace("language", "programming language!");
+
+
+        System.out.println(st1);// out "Java is the greatest programming language!!"
+
+    }
+
+}
