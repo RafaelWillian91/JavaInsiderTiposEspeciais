0441a1dd98807ae19996d9ceaa9ff783fb9826d7
Rafa <rafa.willian91@gmail.com>
2023-02-17 20:19:13 -0300

    Implementando o metodo Intern

--- src/stringBuilder/Stringapp3.java
+++ src/stringBuilder/Stringapp3.java
@@ -3,7 +3,7 @@ package stringBuilder;
 public class Stringapp3 {
 
     public static void main(String[] args) {
-        //Para trabalhar com contatenacao de String
+        //Para trabalhar com contatenacao de String dentro de Loops
         StringBuilder s1 = new StringBuilder();
 
         for(char c = 'A'; c < 'Z'; c++){
